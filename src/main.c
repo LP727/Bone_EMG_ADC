769dd4794101d349d5de8147dcbaae074395b2c7
LP727 <lpbeliveau.f@gmail.com>
2020-05-24 13:19:51 -0400

    Removed useless comment and added converstion factor for display. Next step: still resolution augmentation

--- src/main.c
+++ src/main.c
@@ -35,6 +35,7 @@
 #define SEC_IN_BUF  4           //!< Number of seconds of acquisition in buffer
 #define NANO_IN_SEC 1000000000  //!< Number of nanoseconds in a second
 #define BUFFER_SIZE (SEC_IN_BUF * ACQ_RATE_HZ)
+#define ADC_TO_MV   36          //!< Convertion factor to go from adc (16bits) to ref voltage (1800mV)
 
 /////////////////////////////////////////////////
 /// ... Stub ...
@@ -207,7 +208,6 @@ uint32_t ADC_acquisition_start(struct adc_s *actAdc)
 */
 uint32_t ADC_display(struct adc_s *actAdc)
 {
-    //TODO: Fill in display function
     //!< This function retrieves the latest x samples from
     //!< the ADC driver and stores and displays them in
     //!< a target rate of 10 updates for seconds.
@@ -238,7 +238,7 @@ uint32_t ADC_display(struct adc_s *actAdc)
             {
                 dispInd = (i * 100);
                 dispInd = (dispInd + (((secInd+1) % SEC_IN_BUF) * ACQ_RATE_HZ)) % BUFFER_SIZE;
-                printf("%d",ADC_buffer[dispInd]); //!< adjust index to move the display as new data comes in
+                printf("%d",ADC_buffer[dispInd]/ADC_TO_MV); //!< adjust index to move the display as new data comes in
             }
             fflush(stdout);
             swap(&actAdc->pStart,&actAdc->pMid);
